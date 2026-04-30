98e68e35ce3ad4aaaca7f12d640a0b45a8394d40
YS7538 <155005980+YS7538@users.noreply.github.com>
2026-04-30 21:51:42 +0530

    A hyperparameter sweep over hidden layer sizes revealed that smaller architectures (5 neurons) achieved the best generalization performance. Increasing model capacity beyond this led to overfitting, as evidenced by increasing training accuracy without corresponding improvements in test accuracy.
    Tried different number of neurons in the hidden layer whilst keeping the no. of hidden layers exactly one. Now im gonna try different combinations of small no. of neurons over multiple layers

--- src/ANN.py
+++ src/ANN.py
@@ -7,7 +7,6 @@ X_train,y_train,X_test,y_test= get_data()
 losses= []
 #Initializing parameters
 def parameters(X_n,y_n,h_n):
-    np.random.seed(100)
     
     W1= np.random.randn(h_n,X_n)
     b1= np.zeros((h_n,1))
@@ -54,66 +53,74 @@ def Loss(A2,y):
     return loss
 
 X_n= X_train.shape[0]
-h_n= 10
+layers= [4,5,6,7,8,9,10]
 y_n=1
 
-W1,b1,W2,b2= parameters(X_n,y_n,h_n)
 
-
-#Backpropogation
-
-def backprop(X,y,Z1,A1,Z2,A2,W2):
-    m = X.shape[1]
+for h_n in layers:
     
-    dZ2 = A2 - y
-    dW2 = (1/m) * np.dot(dZ2, A1.T)
-    db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
+    np.random.seed(100)
+    W1,b1,W2,b2= parameters(X_n,y_n,h_n)
+
+
+    #Backpropogation
+
+    def backprop(X,y,Z1,A1,Z2,A2,W2):
+        m = X.shape[1]
+        
+        dZ2 = A2 - y
+        dW2 = (1/m) * np.dot(dZ2, A1.T)
+        db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
+        
+        dZ1 = np.dot(W2.T, dZ2) * (Z1 > 0)
+        dW1 = (1/m) * np.dot(dZ1, X.T)
+        db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
+        
+        return dW1, db1, dW2, db2
+
+    def update(W1,b1,W2,b2,dW1,db1,dW2,db2,lr):
+        W1 -= lr * dW1
+        b1 -= lr * db1
+        W2 -= lr * dW2
+        b2 -= lr * db2
+        
+        return W1, b1, W2, b2
+
+    epochs = 1000
+    lr = 0.01
+
+    for i in range(epochs):
+        Z1, A1, Z2, A2 = forward_pass(X_train, W1, b1, W2, b2)
+        
+        loss = Loss(A2, y_train)
+        losses.append(loss)
+        
+        dW1, db1, dW2, db2 = backprop(X_train, y_train, Z1, A1, Z2, A2, W2)
+        
+        W1, b1, W2, b2 = update(W1, b1, W2, b2, dW1, db1, dW2, db2, lr)
+        
+        #if i % 100 == 0:
+        #    print("Loss:", loss)
+
+
+    _, _, _, A2_train = forward_pass(X_train, W1, b1, W2, b2)
+    train_preds = (A2_train > 0.5).astype(int)
+    train_acc = np.mean(train_preds == y_train)
+
+    _, _, _, A2_test = forward_pass(X_test, W1, b1, W2, b2)
+    test_preds = (A2_test > 0.5).astype(int)
+    test_acc = np.mean(test_preds == y_test)
+
+    print(f"Train Accuracy for h_n= {h_n}:", train_acc * 100)
+    print(f"Train Accuracy for h_n= {h_n}:", test_acc * 100)
+    print()
     
-    dZ1 = np.dot(W2.T, dZ2) * (Z1 > 0)
-    dW1 = (1/m) * np.dot(dZ1, X.T)
-    db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
-    
-    return dW1, db1, dW2, db2
-
-def update(W1,b1,W2,b2,dW1,db1,dW2,db2,lr):
-    W1 -= lr * dW1
-    b1 -= lr * db1
-    W2 -= lr * dW2
-    b2 -= lr * db2
-    
-    return W1, b1, W2, b2
-
-epochs = 1000
-lr = 0.01
-
-for i in range(epochs):
-    Z1, A1, Z2, A2 = forward_pass(X_train, W1, b1, W2, b2)
-    
-    loss = Loss(A2, y_train)
-    losses.append(loss)
-    
-    dW1, db1, dW2, db2 = backprop(X_train, y_train, Z1, A1, Z2, A2, W2)
-    
-    W1, b1, W2, b2 = update(W1, b1, W2, b2, dW1, db1, dW2, db2, lr)
-    
-    if i % 100 == 0:
-        print("Loss:", loss)
+    #plot losses(skipping for now)
+    '''plt.plot(losses)
+    plt.xlabel("Epochs")
+    plt.ylabel("loss")
+    plt.title("Epochs vs loss")
+    plt.show()'''
+    losses.clear()
 
 
-_, _, _, A2_train = forward_pass(X_train, W1, b1, W2, b2)
-train_preds = (A2_train > 0.5).astype(int)
-train_acc = np.mean(train_preds == y_train)
-
-_, _, _, A2_test = forward_pass(X_test, W1, b1, W2, b2)
-test_preds = (A2_test > 0.5).astype(int)
-test_acc = np.mean(test_preds == y_test)
-
-print("Train Accuracy:", train_acc * 100)
-print("Test Accuracy:", test_acc * 100)
-
-#plot losses
-plt.plot(losses)
-plt.xlabel("Epochs")
-plt.ylabel("loss")
-plt.title("Epochs vs loss")
-plt.show()
